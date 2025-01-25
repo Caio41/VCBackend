0432b78836076c9907964ea19ad90436dcaedb63
Caio <caiaoviniciuscc@gmail.com>
2025-01-25 18:15:38 -0300

    feat: add video no db

--- routes/videos.py
+++ routes/videos.py
@@ -1,22 +1,41 @@
-from fastapi import APIRouter, File, UploadFile
+from fastapi import APIRouter, File, Form, UploadFile
+from database.models import Video, VideoPublic
 from service.videos_service import upload_file, list_files, get_video_with_key
+from database.utils import SessionDep
 
 router = APIRouter()
 
-@router.get('')
+
+@router.get("/")
 def get_all_videos():
     response = list_files()
     return response
 
 
-@router.post('')
-async def upload_video(file: UploadFile = File(...)):
-    await upload_file(file)
-    return f'file: {file.filename}.'
+@router.post("/")
+async def upload_video(
+    db: SessionDep,
+    titulo: str = Form(...),
+    descricao: str = Form(...),
+    usuario_id: int = Form(...),
+    file: UploadFile = File(...),
+) -> VideoPublic:
+    upload_data = await upload_file(file)
+    file_key = upload_data['file_key']
+
+    video = Video(
+        titulo=titulo,
+        descricao=descricao,
+        usuario_id=usuario_id,
+        url=file_key,
+    )
+
+    db.add(video)
+    db.commit()
+
+    return video
 
 
-
-@router.get('/{video_key}')
+@router.get("/{video_key}")
 def get_video(key: str):
     return get_video_with_key(key)
-    
