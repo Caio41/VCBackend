3453369b66b0a8ac34d3451734f22e97f4eb4d7a
Caio <caiaoviniciuscc@gmail.com>
2025-01-25 18:14:38 -0300

    fix: conserta usuario relationship e ajusta model video

--- database/models.py
+++ database/models.py
@@ -1,23 +1,31 @@
-from datetime import date
+from datetime import date, datetime, timezone
 from sqlmodel import SQLModel, Field, Relationship
 
 
 class Inscricao(SQLModel, table=True):
-    __tablename__ = 'inscricao'
+    __tablename__ = "inscricao"
 
-    inscrito: int | None = Field(foreign_key='usuario.id', primary_key=True, ondelete='CASCADE')
-    canal: int | None = Field(foreign_key='usuario.id', primary_key=True, ondelete='CASCADE')
+    inscrito: int | None = Field(
+        foreign_key="usuario.id", primary_key=True, ondelete="CASCADE"
+    )
+    canal: int | None = Field(
+        foreign_key="usuario.id", primary_key=True, ondelete="CASCADE"
+    )
 
     # mesma logica de following - followed
+
 
 # -----------------------------------------------------------------------------------------------------------------------------------------------
 # Usuario
 class UsuarioComunidade(SQLModel, table=True):
-    __tablename__ = 'usuario_comunidade'
+    __tablename__ = "usuario_comunidade"
 
-    usuario: int | None = Field(foreign_key='usuario.id', primary_key=True, ondelete='CASCADE') 
-    comunidade: int | None = Field(foreign_key='comunidade.id', primary_key=True, ondelete='CASCADE')
-
+    usuario: int | None = Field(
+        foreign_key="usuario.id", primary_key=True, ondelete="CASCADE"
+    )
+    comunidade: int | None = Field(
+        foreign_key="comunidade.id", primary_key=True, ondelete="CASCADE"
+    )
 
 
 class UsuarioBase(SQLModel):
@@ -28,16 +36,32 @@ class UsuarioBase(SQLModel):
 
 
 class Usuario(UsuarioBase, table=True):
-    __tablename__ = 'usuario'
+    __tablename__ = "usuario"
 
     id: int | None = Field(default=None, primary_key=True)
 
-    videos: list['Video'] = Relationship(back_populates='usuario')
-    comentarios: list['Comentario'] = Relationship(back_populates='usuario')
-    
-    comunidades: list['Comunidade'] = Relationship(link_model=UsuarioComunidade, back_populates='usuarios')
-    inscritos: list['Usuario'] = Relationship(link_model=Inscricao, back_populates='inscricoes')
-    inscricoes: list['Usuario'] = Relationship(link_model=Inscricao, back_populates='inscritos')
+    videos: list["Video"] = Relationship(back_populates="usuario")
+    comentarios: list["Comentario"] = Relationship(back_populates="usuario")
+
+    comunidades: list["Comunidade"] = Relationship(
+        link_model=UsuarioComunidade, back_populates="usuarios"
+    )
+    inscritos: list["Usuario"] = Relationship(
+        link_model=Inscricao,
+        back_populates="inscricoes",
+        sa_relationship_kwargs=dict(
+            primaryjoin="Usuario.id==Inscricao.canal",
+            secondaryjoin="Usuario.id==Inscricao.inscrito",
+        ),
+    )
+    inscricoes: list["Usuario"] = Relationship(
+        link_model=Inscricao, back_populates="inscritos",
+        sa_relationship_kwargs=dict(
+            primaryjoin="Usuario.id==Inscricao.inscrito",
+            secondaryjoin="Usuario.id==Inscricao.canal",
+        ),
+    )
+
 
 # -----------------------------------------------------------------------------------------------------------------------------------------------
 # Comentario
@@ -47,14 +71,14 @@ class ComentarioBase(SQLModel):
 
 
 class Comentario(ComentarioBase, table=True):
-    __tablename__ = 'comentario'
+    __tablename__ = "comentario"
 
     id: int | None = Field(default=None, primary_key=True)
-    video_id: int = Field(foreign_key='video.id')
-    usuario_id: int = Field(foreign_key='usuario.id')
+    video_id: int = Field(foreign_key="video.id")
+    usuario_id: int = Field(foreign_key="usuario.id")
 
-    video: 'Video' = Relationship(back_populates='comentarios')
-    usuario: 'Usuario' = Relationship(back_populates='comentarios')
+    video: "Video" = Relationship(back_populates="comentarios")
+    usuario: "Usuario" = Relationship(back_populates="comentarios")
 
 
 # -----------------------------------------------------------------------------------------------------------------------------------------------
@@ -64,22 +88,25 @@ class ComunidadeBase(SQLModel):
 
 
 class Comunidade(ComunidadeBase, table=True):
-    __tablename__ = 'comunidade'
+    __tablename__ = "comunidade"
 
     id: int | None = Field(default=None, primary_key=True)
 
-    usuarios: list['Usuario'] = Relationship(link_model=UsuarioComunidade, back_populates='comunidades')
+    usuarios: list["Usuario"] = Relationship(
+        link_model=UsuarioComunidade, back_populates="comunidades"
+    )
+
 
 # -----------------------------------------------------------------------------------------------------------------------------------------------
 # Notificacao
 # vou ajeitar depois isso aqui
 class NotificacoesBase(SQLModel):
-    lida: bool 
+    lida: bool
     data: date
 
 
 class Notificacao(NotificacoesBase, table=True):
-    __tablename__ = 'notificacao'
+    __tablename__ = "notificacao"
 
     id: int | None = Field(default=None, primary_key=True)
 
@@ -87,62 +114,80 @@ class Notificacao(NotificacoesBase, table=True):
 # -----------------------------------------------------------------------------------------------------------------------------------------------
 # Videos
 
+
 # Many to Many: Playlist - Video
 class PlaylistVideos(SQLModel, table=True):
-    __tablename__ = 'playlist_videos'
+    __tablename__ = "playlist_videos"
 
-    playlist: int | None = Field(foreign_key='playlist.id', primary_key=True, ondelete='CASCADE') 
-    video: int | None = Field(foreign_key='video.id', primary_key=True, ondelete='CASCADE')
-
+    playlist: int | None = Field(
+        foreign_key="playlist.id", primary_key=True, ondelete="CASCADE"
+    )
+    video: int | None = Field(
+        foreign_key="video.id", primary_key=True, ondelete="CASCADE"
+    )
 
 
 class VideoBase(SQLModel):
     titulo: str
     descricao: str
-    thumb: str
-    visualizacoes: int
-    likes: int
-    duracao: int
-    data: date
-    
+
+
+#    thumb: str
+#    duracao: int
+
 
 class Video(VideoBase, table=True):
-    __tablename__ = 'video'
+    __tablename__ = "video"
 
     id: int | None = Field(default=None, primary_key=True)
-    usuario_id: int = Field(foreign_key='usuario.id')
+    usuario_id: int = Field(foreign_key="usuario.id")
 
-    usuario: 'Usuario' = Relationship(back_populates='videos')
+    visualizacoes: int = Field(default=0)
+    likes: int = Field(default=0)
+    data: date = Field(default_factory=lambda: datetime.now(tz=timezone.utc))
+    url: str
 
-    comentarios: list['Comentario'] = Relationship(back_populates='video')
+    usuario: "Usuario" = Relationship(back_populates="videos")
 
-    playlists: list['Playlist'] = Relationship(link_model=PlaylistVideos ,back_populates='videos')
+    comentarios: list["Comentario"] = Relationship(back_populates="video")
+
+    playlists: list["Playlist"] = Relationship(
+        link_model=PlaylistVideos, back_populates="videos"
+    )
+
+
+# acabei nao usando
+class VideoCreate(VideoBase):
+    usuario_id: int
+
+
+class VideoPublic(VideoBase):
+    id: int
+    url: str
 
 
 # -----------------------------------------------------------------------------------------------------------------------------------------------
 # Playlist
 
-class PlaylistBase(SQLModel):
-    nome: str 
 
+class PlaylistBase(SQLModel):
+    nome: str
 
 
 class Playlist(PlaylistBase, table=True):
-    __tablename__ = 'playlist'
+    __tablename__ = "playlist"
 
     id: int | None = Field(default=None, primary_key=True)
 
-    videos: list['Video'] = Relationship(link_model=PlaylistVideos, back_populates='playlists')
-
-
-
-
+    videos: list["Video"] = Relationship(
+        link_model=PlaylistVideos, back_populates="playlists"
+    )
 
 
 # Video
 # Usuario
 # Comentario
-# Comunidade 
+# Comunidade
 # Inscrições
 # Notificações
 # Playlists
